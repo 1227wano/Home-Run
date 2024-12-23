713279fd323882b41d2511f0e22de29230193e70
1227wano <1227wanho@gmail.com>
2024-12-23 14:53:52 +0900

    와노
    와노 중복테스트

--- HomeRunProject/src/main/java/com/kh/baseball/BaseController.java
+++ HomeRunProject/src/main/java/com/kh/baseball/BaseController.java
@@ -5,7 +5,7 @@ public class BaseController {
 	public void abc() {
 		System.out.println("어쩌고저쩌고");
 		System.out.println("민돌테스트");
-		System.out.println("민돌테스트2");
+		System.out.println("");
 	}
 	
 
