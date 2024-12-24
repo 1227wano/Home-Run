fc763127c58c9251cf4460c2da4c98d0cba2cdc1
1227wano <1227wanho@gmail.com>
2024-12-24 10:52:23 +0900

    선수관련 패키지 및 클래스 생성

---
+++ HomeRunProject/src/main/java/com/kh/baseball/player/model/service/PlayerService.java
@@ -0,0 +1,27 @@
+package com.kh.baseball.player.model.service;
+
+import java.util.List;
+
+import com.kh.baseball.player.model.vo.Player;
+
+public interface PlayerService {
+	
+	
+	// 선수 등록 신청
+	int insertPlayer(Player player);
+
+	// 선수 정보 일람
+	List<Player> selectPlayerBoard();			
+
+	// 선수 정보 상세조회
+	Player selectPlayer(int userNo);
+	
+	// 선수 정보 수정
+	int updatePlayer(Player player);
+	
+	// 선수 정보 삭제
+	int deletePlayer(int userNo);
+	
+	
+
+}
